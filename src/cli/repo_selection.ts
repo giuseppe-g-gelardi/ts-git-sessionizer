6433a1eaabb8a9385fe9a1368bda5424ab75d72b
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2023-10-26 09:41:46 -0400

    install ora for loading spinners

--- src/cli/repo_selection.ts
+++ src/cli/repo_selection.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+
+import ora from 'ora'
 import { select } from "@inquirer/prompts"
 
 type PartialRepo = {
@@ -16,22 +18,31 @@ type OptionsType = {
 // should add ssh_url to the options and prompt the user to choose 
 // between https and ssh
 
-
 async function fetchGithubRepos(token: string): Promise<Array<OptionsType>> {
-  const userResponse = await axios.get('https://api.github.com/user', {
-    headers: { Authorization: `token ${token}` }
-  })
-  const repoResponse = await axios.get(userResponse.data.repos_url, {
-    headers: { Authorization: `token ${token}` }
-  })
+  const spinner = ora('Fetching repositories...').start()
 
-  const repository_list = repoResponse.data.map((r: PartialRepo) => ({
-    name: r.name,
-    value: r.html_url,
-    description: r.description
-  }))
+  try {
 
-  return repository_list
+    const userResponse = await axios.get('https://api.github.com/user', {
+      headers: { Authorization: `token ${token}` }
+    })
+    const repoResponse = await axios.get(userResponse.data.repos_url, {
+      headers: { Authorization: `token ${token}` }
+    })
+
+    const repository_list = repoResponse.data.map((r: PartialRepo) => ({
+      name: r.name,
+      value: r.html_url,
+      description: r.description
+    }))
+
+    spinner.succeed('Repositories fetched successfully!')
+    return repository_list
+  } catch (error) {
+    spinner.fail('Failed to fetch repositories!')
+    console.log(error)
+    process.exit(1)
+  }
 }
 
 export async function repoSelection(token: string): Promise<string> {
--- src/exit.ts
+++ src/exit.ts
@@ -1,30 +1,11 @@
 
-
-
+import ora from 'ora'
 
 export function Exit() {
-  const loadingChars = ['/', '-', '\\', '|'];
-  let i = 0;
-  let timeLeft = 3;
-
-  const loadingInterval = setInterval(() => {
-    process.stdout.clearLine(0);
-    process.stdout.cursorTo(0);
-
-    if (timeLeft > 1) {
-      process.stdout.write(`Exiting... ${loadingChars[i]}`);
-    } else {
-      process.stdout.write(`Bye! 👋 ${loadingChars[i]}`);
-    }
-
-    i = (i + 1) % loadingChars.length;
-    timeLeft--;
-  }, 250);
+  const spinner = ora('Exiting...').start()
 
   setTimeout(() => {
-    clearInterval(loadingInterval);
-    process.stdout.clearLine(0);
-    process.stdout.cursorTo(0);
+    spinner.succeed('Bye! 👋');
     process.exit(0);
   }, 3000);
 }
