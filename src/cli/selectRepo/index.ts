1092cde4a6891a280012e149e527d89ff8b52da3
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2023-10-29 09:37:31 -0400

    add .env.example, add field to show if specific repo is public or private, testing with finegrained github token to fetch all repos, including private repos

--- src/cli/selectRepo/index.ts
+++ src/cli/selectRepo/index.ts
@@ -8,6 +8,7 @@ type PartialRepo = {
   name: string,
   html_url: string,
   description: string
+  private: boolean
 }
 
 type OptionsType = {
@@ -26,24 +27,71 @@ export function getRepoNameFromUrl(url: string): string {
 }
 
 export async function fetchGithubRepos(token: string): Promise<Array<OptionsType>> {
-  const spinner = ora('Fetching your repositories...').start()
-  const headers = { Authorization: `token ${token}` }
+  const spinner = ora('Fetching your repositories...').start();
+  const headers = { Authorization: `token ${token}` };
 
   try {
-    const userResponse = await axios.get('https://api.github.com/user', { headers })
-    const repoResponse = await axios.get(userResponse.data.repos_url, { headers })
+    const userResponse = await axios.get('https://api.github.com/user', { headers });
+    const reposUrl = userResponse.data.repos_url;
+    // const reposUrl = 'https:/api.github.com/user/repos'
+    // const reposUrl = `https://api.github.com/search/repositories?q=user:${userResponse.data.login.replace(/['"]+/g, '')}}`
+    let allRepositories: OptionsType[] = [];
+    let page = 1;
+    let hasMorePages = true;
 
-    const repository_list = repoResponse.data.map((r: PartialRepo) => ({
-      name: r.name,
-      value: r.html_url,
-      description: r.description
-    }))
+    while (hasMorePages) {
+      const repoResponse = await axios.get(reposUrl, {
+        headers,
+        params: { page, per_page: 100, type: 'all' }, // Adjust per_page as needed
+      });
 
-    spinner.succeed('Repositories fetched successfully!')
-    return repository_list
+      const repositories = repoResponse.data.map((r: PartialRepo) => ({
+        name: r.name && `${r.name} -- ${r.private ? '(private)' : '(public)'}`,
+        value: r.html_url,
+        description: r.description,
+      }));
+
+      allRepositories = allRepositories.concat(repositories);
+
+      if (repoResponse.headers.link && repoResponse.headers.link.includes('rel="next"')) {
+        // If there is a "next" link in the response headers, there are more pages.
+        page++;
+      } else {
+        hasMorePages = false; // No more pages to fetch
+      }
+    }
+
+    spinner.succeed('Repositories fetched successfully!');
+    return allRepositories;
   } catch (error) {
-    spinner.fail('Failed to fetch repositories!')
-    console.log(error)
-    process.exit(1)
+    spinner.fail('Failed to fetch repositories!');
+    console.error(error);
+    process.exit(1);
   }
 }
+
+
+
+
+// export async function fetchGithubRepos(token: string): Promise<Array<OptionsType>> {
+//   const spinner = ora('Fetching your repositories...').start()
+//   const headers = { Authorization: `token ${token}` }
+//
+//   try {
+//     const userResponse = await axios.get('https://api.github.com/user', { headers })
+//     const repoResponse = await axios.get(userResponse.data.repos_url, { headers })
+//
+//     const repository_list = repoResponse.data.map((r: PartialRepo) => ({
+//       name: r.name,
+//       value: r.html_url,
+//       description: r.description
+//     }))
+//
+//     spinner.succeed('Repositories fetched successfully!')
+//     return repository_list
+//   } catch (error) {
+//     spinner.fail('Failed to fetch repositories!')
+//     console.log(error)
+//     process.exit(1)
+//   }
+// }
--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,55 @@ import { initCli } from './cli'
 // ! ********************************************************************* ! //
 //                                                                           //
 // ! ********************************************************************* ! //
+
+// import dotenv from 'dotenv'
+
+// dotenv.config()
+// const token = process.env.GITHUB_TOKEN
+
+// // (async function main() {
+
+// //   const repos = await fetch('https://api.github.com/user/repos', {
+// //     headers: {
+// //       Authorization: `token ${"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"}`,
+// //     }
+// //   }).then(res => res.json())
+
+// //   console.log(repos.length)
+
+// // })()
+
+// async function getAllRepositories<T>() {
+//   const perPage = 100; // Number of repositories to fetch per page
+//   let page = 1;
+//   let allRepos: T[] = [];
+
+//   while (true) {
+//     const url = `https://api.github.com/user/repos?page=${page}&per_page=${perPage}&visibility=all`;
+//     const response = await fetch(url, { headers: { Authorization: `token ${token}` } });
+//     const repos = await response.json();
+
+//     if (repos.length === 0) {
+//       break; // No more repositories to fetch
+//     }
+
+//     allRepos = allRepos.concat(repos);
+//     page++;
+//   }
+//   return allRepos;
+// }
+
+// (async function main() {
+//   const allRepos = await getAllRepositories();
+
+//   console.log(`Total number of repositories: ${allRepos.length}`);
+//   console.log(`repo`, allRepos[0])
+
+//   // if (allRepos.length > 0) {
+//   //   console.log("List of repositories:");
+//   //   // allRepos.forEach(repo => {});
+//   // } else {
+//   //   console.log("You have no repositories.");
+//   // }
+// })();
+
