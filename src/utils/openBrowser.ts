9e929214a2410e95e0a095349237131ebcabd338
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2023-10-31 13:47:30 +0000

    update openBrowser function to check users platform and run the correct command to open the users browser window

--- src/utils/openBrowser.ts
+++ src/utils/openBrowser.ts
@@ -2,13 +2,47 @@ import { exec } from 'child_process';
 
 export function openBrowser(url: string): Promise<void> {
   return new Promise((resolve, reject) => {
-    exec(`open ${url}`, (error) => {
+    let openCommand: string;
+
+    // if (process.platform === 'darwin') {
+    //   openCommand = `open ${url}`;
+    // } else if (process.platform === 'win32') {
+    //   openCommand = `start ${url}`;
+    // } else {
+    //   openCommand = `xdg-open ${url}`;
+    // }
+
+    switch (process.platform) {
+      case 'darwin': openCommand = `open ${url}`; break;
+      case 'win32': openCommand = `start ${url}`; break;
+      case 'linux': openCommand = `xdg-open ${url}`; break;
+      default : openCommand = `open ${url}`; break;
+    }
+
+    exec(openCommand, (error) => {
       if (error) {
         console.error(`Error opening the browser: ${error}`);
         reject(error);
       } else {
         resolve();
       }
-    });
+    }
+    );
   });
 }
+
+
+
+
+
+
+    // exec(`open ${url}`, (error) => {
+    //   if (error) {
+    //     console.error(`Error opening the browser: ${error}`);
+    //     reject(error);
+    //   } else {
+    //     resolve();
+    //   }
+    // });
+//   });
+// }
